afc2d8954d2f22f8f787b4d96aada3f2c98299e6
Zain Memon <zain@inzain.net>
2013-07-02 14:33:54 -0700

    Bump version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='rq-scheduler',
-    version='0.3.6',
+    version='0.3.7',
     author='Selwin Ong',
     author_email='selwin.ong@gmail.com',
     packages=['rq_scheduler'],
